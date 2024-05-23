c45738e246ce6e3c619d78d56052510b156d2fe9
Maxim Shapovalov <Maxim.A.Shapovalov@gmail.com>
2024-05-23 18:13:15 +0300

    add any fitches

--- src/ru/gb/hw4/Main.java
+++ src/ru/gb/hw4/Main.java
@@ -3,10 +3,12 @@ package ru.gb.hw4;
 public class Main {
     public static void main(String[] args) {
 
-        Person person1 = new Person(42, "Сергей", Gender.Male);
-        Person person2 = new Person(35, "Валентина", Gender.Female);
-        Person person3 = new Person(29, "Инокентий", Gender.Male);
-        Person[] people = {person1, person2, person3};
+        Person person1 = new Person(42, "Sergey", Gender.Male);
+        Person person2 = new Person(35, "Valentina", Gender.Female);
+        Person person3 = new Person(29, "Inokenty", Gender.Male);
+        Person person4 = new Person(34, "Ashot", Gender.Male);
+        Person person5 = new Person(29, "Goar", Gender.Female);
+        Person[] people = {person1, person2, person3, person4, person5};
 
 
         for (Person person : people) {
